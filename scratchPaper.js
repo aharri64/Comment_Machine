fd2ba48148549935acadab9755a5d87386f3a5e3
aharri64 <haveacompleteday@gmail.com>
2021-02-17 20:45:37 -0700

    troubleshooting auth

--- scratchPaper.js
+++ scratchPaper.js
@@ -1,4 +1,8 @@
+/*
+const express = require('express')
 var axios = require("axios").default;
+
+const app = express();
 
 var options = {
   method: 'GET',
@@ -31,3 +35,22 @@ var options = {
   }).catch(function (error) {
       console.error(error);
   });
+
+app.get('/results', (req, res) => {
+    let searchByName = 's=' + req.body.name
+    // let searchByIngredient = 'i=' req.body.ingredient
+    var qs= {
+        headers:{
+            'x-rapidapi-key': process.env.RAPID_API_KEY,
+            'x-rapidapi-host': process.env.RAPID_API_HOST 
+        }
+    }
+
+    axios.get(`https://the-cocktail-db.p.rapidapi.com/search.php?${searchByName}`, qs)
+    .then(function (response) {
+        let data = responce.data.search
+        res.render('results', {data})
+    })
+
+})
+*/
